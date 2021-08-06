be1744b273a5a9eca8bdbf73d454d8ec83cf1916
yaman1337 <yaman.sarabariya@gmail.com>
2021-08-06 15:17:05 +0545

    routing issue fixed

--- utils/requests.js
+++ utils/requests.js
@@ -7,42 +7,42 @@ export default {
     },
     fetchTopRated: {
         title: "Top Rated",
-        url: ""
+        url:  `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
     },
     fetchAction: {
         title: "Action",
-        url: ""
+        url: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
     },
     fetchComedy: {
         title: "Comedy",
-        url: ""
+        url: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
     },
     fetchHorror: {
         title: "Horror",
-        url: ""
+        url: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
     },
     fetchRomance: {
         title: "Romance",
-        url: ""
+        url:`/discover/movie?api_key=${API_KEY}&with_genres=10749`,
     },
     fetchMystery: {
         title: "Mystery",
-        url: ""
+        url: `/discover/movie?api_key=${API_KEY}&with_genres=9648`,
     },
     fetchSciFi: {
         title: "Sci-Fi",
-        url: ""
+        url:  `/discover/movie?api_key=${API_KEY}&with_genres=878`,
     },
     fetchWestern: {
         title: "Western",
-        url: ""
+        url:  `/discover/movie?api_key=${API_KEY}&with_genres=37`,
     },
     fetchAnimation: {
         title: "Animation",
-        url: ""
+        url:  `/discover/movie?api_key=${API_KEY}&with_genres=16`,
     },
     fetchTvMovie: {
         title: "Tv Movie",
-        url: ""
+        url: `/discover/movie?api_key=${API_KEY}&with_genres=10770`,
     }
 }
